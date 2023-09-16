7f89db85933191a479e2b00ece7910d5d3574787
Hiroshi Tokumaru <github@tokumaru.org>
2023-09-16 22:45:51 +0900

    デバッグ表示を削除

--- html/login.php
+++ html/login.php
@@ -8,10 +8,8 @@ $options = [
 try {
     $pdo = new PDO("sqlite:../db.sqlite3", null, null, $options);
     $sql = "SELECT * FROM users WHERE userid = '$userid'";
-// var_dump($sql);    
     $stmt = $pdo->query($sql);
     $user = $stmt->fetch();
-// var_dump($user);
     if ($user && password_verify($password, $user['password'])) {
         echo "ログイン成功:" . htmlspecialchars($user['userid']);
     } else {
